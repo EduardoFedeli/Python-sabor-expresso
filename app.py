bb21e1db5ed47438692dcbf6479a83e5d71a6de0
Eduardo Fedeli <dudufedeli@outlook.com>
2025-08-20 09:16:00 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+print('𝕊𝕒𝕓𝕠𝕣 𝔼𝕩𝕡𝕣𝕖𝕤𝕤𝕠')
+
+
+
+
+
+
+print('1, Cadastrar Restaurante')
+print('2. Listar Restaurante')
+print('3. Ativar Restaurante')
+print('4. Sair\n')
+
+opcaoa_escolhida = input('Escolha uma opçãõ: ')
+
