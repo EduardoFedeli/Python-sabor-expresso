dc993166082d3b9d8ae526e06551725476837706
Eduardo Fedeli <dudufedeli@outlook.com>
2025-08-20 09:22:00 -0300

    Segundo commit

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 print('𝕊𝕒𝕓𝕠𝕣 𝔼𝕩𝕡𝕣𝕖𝕤𝕤𝕠')
 
-
-
-
-
-
 print('1, Cadastrar Restaurante')
 print('2. Listar Restaurante')
 print('3. Ativar Restaurante')
@@ -12,3 +7,11 @@ print('4. Sair\n')
 
 opcaoa_escolhida = input('Escolha uma opçãõ: ')
 
+if opcaoa_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcaoa_escolhida == 2:
+    print('Listar restaurantes')
+elif opcaoa_escolhida == 3:
+    print('Ativar Restaurante')
+else:
+    print('Encerrando o programa')
